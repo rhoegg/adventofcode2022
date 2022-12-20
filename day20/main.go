2366d7a78663c532657ff900511bd34d5c8e8b5f
Ryan Hoegg <ryan.hoegg@gmail.com>
2022-12-20 01:07:29 -0600

    day 20 part 2 in go

--- day20/main.go
+++ day20/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 type MixNumber struct {
-	Number int
+	Number int64
 	OriginalPosition int
 }
 
 func (n MixNumber) String() string {
-	return strconv.Itoa(n.Number)
+	return strconv.FormatInt(n.Number, 10)
 }
 
 func mix(list []MixNumber) []MixNumber {
@@ -26,20 +26,21 @@ func mix(list []MixNumber) []MixNumber {
 				mover := list[j]
 				next := make([]MixNumber, len(list))
 				relativeMove := mover.Number
-				for relativeMove > len(list) - 1 {
-					relativeMove -= len(list) - 1
+				if relativeMove > int64(len(list) - 1) {
+					relativeMove -= (relativeMove / int64(len(list) - 1)) * int64(len(list) - 1)
 				}
-				for relativeMove < 0 {
-					relativeMove += len(list) - 1
+				if relativeMove < 0 {
+					relativeMove -= (relativeMove / int64(len(list) - 1) - 1) * int64(len(list) - 1)
 				}
-				for k := 0; k < relativeMove; k++ {
+
+				for k := 0; k < int(relativeMove); k++ {
 					next[(k+j) % len(list)] = list[(k+j+1) % len(list)]
 				}
-				next[(relativeMove+j) % len(list)] = mover
-				for k := relativeMove + 1; k < len(list); k++ {
+				next[(int(relativeMove)+j) % len(list)] = mover
+				for k := int(relativeMove) + 1; k < len(list); k++ {
 					next[(k+j) % len(list)] = list[(k+j) % len(list)]
 				}
-				if j + relativeMove > len(list) {
+				if j + int(relativeMove) > len(list) {
 					next = append(next[len(next)-1:], next[0:len(next)-1]...)
 				}
 				list = next
@@ -51,18 +52,29 @@ func mix(list []MixNumber) []MixNumber {
 	return list
 }
 
-func LocateCoordinate(list []MixNumber, i int) int {
+func LocateCoordinate(list []MixNumber, i int) int64 {
 	var zeroLoc int
 	for {
 		if list[zeroLoc].Number == 0 {
 			break
 		}
 		zeroLoc++
-		if zeroLoc > len(list) {
+		if zeroLoc >= len(list) {
 			panic("can't find zero in list")
 		}
 	}
 	return list[(zeroLoc + i) % len(list)].Number
+}
+
+func ApplyDecryptionKey(key int64, message []MixNumber) []MixNumber {
+	result := make([]MixNumber, len(message))
+	for i, n := range message {
+		result[i] = MixNumber{
+			Number: n.Number * key,
+			OriginalPosition: n.OriginalPosition,
+		}
+	}
+	return result
 }
 
 func main() {
@@ -72,13 +84,24 @@ func main() {
 	}
 	var list []MixNumber
 	for i, l := range strings.Split(string(coordinateFile), "\n") {
-		num, _ := strconv.Atoi(l)
+		num, _ := strconv.ParseInt(l, 10, 64)
 		list = append(list, MixNumber{Number: num, OriginalPosition: i})
 	}
-	//fmt.Printf("before %v \n", list)
+	fmt.Printf("before %v \n", list)
 	mixed := mix(list)
-	//fmt.Printf("after %v\n", list)
+	fmt.Printf("after %v\n", list)
 	fmt.Printf("coords: %d, %d, %d\n",
 		LocateCoordinate(mixed, 1000), LocateCoordinate(mixed, 2000), LocateCoordinate(mixed, 3000))
 	fmt.Printf("Part 1: %d\n", LocateCoordinate(mixed, 1000) + LocateCoordinate(mixed, 2000) + LocateCoordinate(mixed, 3000))
+
+	workingMessage := ApplyDecryptionKey(811589153, list)
+	part2Mixed := mix(workingMessage)
+	for i := 0; i < 9; i++ {
+		part2Mixed = mix(part2Mixed)
+	}
+	fmt.Printf("coords: %d, %d, %d\n",
+		LocateCoordinate(part2Mixed, 1000), LocateCoordinate(part2Mixed, 2000), LocateCoordinate(part2Mixed, 3000))
+	fmt.Printf("Part 2: %d\n", LocateCoordinate(part2Mixed, 1000) + LocateCoordinate(part2Mixed, 2000) + LocateCoordinate(part2Mixed, 3000))
+
+
 }
