bce180875c2bf17858c1b1ab1f5864e8243a4c63
Ryan Hoegg <ryan.hoegg@gmail.com>
2023-02-04 23:03:12 -0600

    day 2 part 2 in clojure

--- day2/src/day2/core.clj
+++ day2/src/day2/core.clj
@@ -17,6 +17,16 @@
     :else 0 ; loss
     ))
 
+(defn cheat-play-score
+  [opponent result]
+  (let [opponent-index (opponent-play opponent)
+        cheat-play-index (condp = result
+                           \Y opponent-index ; draw
+                           \X (mod (- opponent-index 1) 3) ; lose
+                           \Z (mod (+ opponent-index 1) 3))] ; win
+
+       (+ cheat-play-index 1))) ; the play score is one more than the index
+
 (defn score-part1
   [turn]
   (let [mine (get (last turn) 0)
@@ -30,10 +40,38 @@
     (+ (score-game (opponent-play opponent) (me-play mine)) shape-score)
     ))
 
-(defn part1
+(defn score-part2
+  [turn]
+  (let [result (get (last turn) 0)
+        opponent (get (first turn) 0)
+        game-score (condp = result
+         \X 0 ; lose
+         \Y 3 ; draw
+         \Z 6 ; win
+         :else -1
+         )]
+     (+ (cheat-play-score opponent result) game-score)
+    ))
+
+(defn parse-input
   [file-name]
   (->>
    (s/split (slurp file-name) #"\n")
-   (map #(s/split % #" "))
+   (map #(s/split % #" ")))
+  )
+
+(defn part1
+  [file-name]
+  (->>
+   file-name
+   (parse-input)
    (map score-part1)
+   (reduce +)))
+
+(defn part2
+  [file-name]
+  (->>
+   file-name
+   (parse-input)
+   (map score-part2)
    (reduce +)))
