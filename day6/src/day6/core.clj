60cddc4194e007ff45b8f9b9476b8418a7c04e15
Ryan Hoegg <ryan.hoegg@gmail.com>
2023-02-10 20:35:10 -0600

    day 6 part 2 in clojure

--- day6/src/day6/core.clj
+++ day6/src/day6/core.clj
@@ -3,21 +3,28 @@
 
 
 (defn unique-chars-in-header
-  [signal]
+  ([signal header-size]
     (->
      signal
-     (subs 0 4)
+     (subs 0 header-size)
      (set)
-     (count)))
+     (count))))
 
-(defn find-start-of-packet
-  ([signal] (find-start-of-packet signal 0))
+(defn find-unique-window
+    ([size signal] (find-unique-window size signal 0))
 
-  ([signal noiseCount]
-   (if (= 4 (unique-chars-in-header signal))
-     (+ noiseCount 4) ; found it
-     (find-start-of-packet (subs signal 1) (inc noiseCount)))))
+    ([size signal noiseCount]
+     (if (= size (unique-chars-in-header signal size))
+       (+ noiseCount size) ; found it
+       (find-unique-window size (subs signal 1) (inc noiseCount)))))
+
+(defn find-start-of-packet [signal] (find-unique-window 4 signal))
+(defn find-start-of-message [signal] (find-unique-window 14 signal))
 
 (defn part1
   [file-name]
   (find-start-of-packet (slurp file-name)))
+
+(defn part2
+  [file-name]
+  (find-start-of-message (slurp file-name)))
